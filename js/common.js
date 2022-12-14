bd31dd4ef792f8d80fb7b0439294e8e0fd21eaa5
Denis <chernousovdv@gmail.com>
2022-12-14 14:02:11 +0700

    адаптив

--- js/common.js
+++ js/common.js
@@ -1,18 +1,40 @@
 $(document).ready(function () {
-	$('.rental__slider').slick({
+	$('.rental__slider,.villas__inner').slick({
 		infinite: true,
 		slidesToShow: 2,
 		slidesToScroll: 2,
 		respondTo: 'window',
 		prevArrow: "<img src='images/right.svg' class='prev' alt='1'>",
-		nextArrow: "<img src='images/right.svg' class='next' alt='2'>"
-	});
-	$('.villas__inner').slick({
-		infinite: true,
-		slidesToShow: 2,
-		slidesToScroll: 2,
-		respondTo: 'window',
-		prevArrow: "<img src='images/right.svg' class='prev' alt='1'>",
-		nextArrow: "<img src='images/right.svg' class='next' alt='2'>"
+		nextArrow: "<img src='images/right.svg' class='next' alt='2'>",
+		responsive: [
+			{
+				breakpoint: 421,
+				settings: {
+					infinite: true,
+					slidesToShow: 1,
+					slidesToScroll: 1
+				
+				}
+			}
+		]
+	// });
+	// $('.villas__inner').slick({
+	// 	infinite: true,
+	// 	slidesToShow: 2,
+	// 	slidesToScroll: 2,
+	// 	respondTo: 'window',
+	// 	prevArrow: "<img src='images/right.svg' class='prev' alt='1'>",
+	// 	nextArrow: "<img src='images/right.svg' class='next' alt='2'>"
+	// 	responsive: [
+	// 		{
+	// 			breakpoint: 414,
+	// 			settings: {
+	// 				infinite: true,
+	// 				slidesToShow: 1,
+	// 				slidesToScroll: 1
+				
+	// 			}
+	// 		}
+	// 	]
 	});
 });
